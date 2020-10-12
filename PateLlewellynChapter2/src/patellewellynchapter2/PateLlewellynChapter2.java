43cfbc68e5bda259b83fc44690f1166cae86c065
adtr0344 <70340152+adtr0344@users.noreply.github.com>
2020-10-12 12:16:00 -0500

    Add files via upload

---
+++ PateLlewellynChapter2/src/patellewellynchapter2/PateLlewellynChapter2.java
@@ -0,0 +1,13 @@
+package patellewellynchapter2;
+public class PateLlewellynChapter2 {
+    public static void main(String[] args) {
+    System.out.println("X X # # # # X X");
+    System.out.println("X # X X X X # X");
+    System.out.println("# X # X X # X #");
+    System.out.println("# X X X X X X #");
+    System.out.println("# X # X X # X #");
+    System.out.println("# X X # # X X #");
+    System.out.println("X # X X X X # X");
+    System.out.println("X X # # # # X X");
+         }   
+}
